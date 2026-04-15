c19989a8d47ccdc2eff69bd3ed49733be49e922c
paulka320 <paulacou4@gmail.com>
2026-04-15 23:26:59 +0300

    included models.py

--- ILES_G6_PROJECT/users/models.py
+++ ILES_G6_PROJECT/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('student','Student'),
+        ('supervisor','Supervisor'),
+        ('academic','Academic Supervisor'),
+        ('admin','Admin'),
+    )
