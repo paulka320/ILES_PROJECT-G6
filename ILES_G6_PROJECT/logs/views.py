52b4d33bcffc5b73721e3964472dc82a443f4506
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-04-16 10:59:56 +0300

    Add imports for viewsets and decorators in views.py

--- ILES_G6_PROJECT/logs/views.py
+++ ILES_G6_PROJECT/logs/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import weeklyLog
+
