8731c799f1c25bdc28928c8ed0622c9016f588a6
nushifah <knushifah@gmail.com>
2026-04-16 21:34:42 +0300

    Add EvaluationCriteria model

--- ILES_G6_PROJECT/evaluations/models.py
+++ ILES_G6_PROJECT/evaluations/models.py
@@ -3,3 +3,6 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
+class EvaluationCriteria(models.model):
+    name = models.CharField(max_length = 100)
+    weight = models.FloatField()
