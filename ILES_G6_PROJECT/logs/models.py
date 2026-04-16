697895a554d9e90361b50df4341a7eba93167c8f
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-04-16 11:58:06 +0300

    Update models.py

--- ILES_G6_PROJECT/logs/models.py
+++ ILES_G6_PROJECT/logs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+user = settings.AUTH-USER_MODEL
+
+class WeeklyLog(models.model):
+  STATUS_CHOICE = (
+    ('draft', 'draft'),
+    ('submitted', 'submitted'),
+    ('reviewed', 'reviewed'),
+    ('approved', 'approved'),
+  )
+    
