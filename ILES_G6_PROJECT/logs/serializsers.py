5526a13b03391c36adbcd06eaf1e0aa1d7499e7e
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-04-16 11:48:03 +0300

    Update serializsers.py

--- ILES_G6_PROJECT/logs/serializsers.py
+++ ILES_G6_PROJECT/logs/serializsers.py
@@ -4,3 +4,8 @@ from .models import WeeklyLog
 class WeeklyLogSerializser(serializers.ModelSerializer):
   class meta:
     model = WeeklyLog
+    fields ='__all__'
+    read_only_fields = ['status', 'student']
+
+def create(self, validated_data):
+  
