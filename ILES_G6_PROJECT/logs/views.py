be392a1e735490fd1a758c226ab0ead3332c0d8a
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-04-16 11:02:14 +0300

    Update views.py

--- ILES_G6_PROJECT/logs/views.py
+++ ILES_G6_PROJECT/logs/views.py
@@ -5,4 +5,6 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import weeklyLog
+from .serializers import WeeklyLogSerializer
+
 
