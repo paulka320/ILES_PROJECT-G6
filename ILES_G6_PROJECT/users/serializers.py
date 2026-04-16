503311118d7316af10021b7b46ab2300b2ee4096
paulka320 <paulacou4@gmail.com>
2026-04-16 10:14:29 +0300

    improving serializers.py

--- ILES_G6_PROJECT/users/serializers.py
+++ ILES_G6_PROJECT/users/serializers.py
@@ -7,3 +7,17 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['id','username','email','role']
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username','email','password','role']
+
+        def create(self,validated_data):
+            user = CustomUser.objects.create_user(
+                username=validated_data['username'],
+                email = validated_data['email'],
+                role = validated_data['role'],
+                password = validated_data['password']
+            )
