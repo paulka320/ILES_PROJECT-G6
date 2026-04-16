3f67db7aa333ee166ab66400ad7bf00129549f75
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-04-16 11:40:38 +0300

    Register WeeklyLogViewSet with the router

--- ILES_G6_PROJECT/logs/urls.py
+++ ILES_G6_PROJECT/logs/urls.py
@@ -2,3 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import WeeklyLogViewSet
 
 router =DefaultRouter()
+router.register(r'logs', weeklyLogViewSet)
+
+urlpatterns = router.urls
