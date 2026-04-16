b67f4c43eb4e29431dd62b630c890c2ebdd66b31
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-04-16 11:53:27 +0300

    Add validation to prevent editing approved logs
    Prevent editing of log entries once approved.

--- ILES_G6_PROJECT/logs/serializsers.py
+++ ILES_G6_PROJECT/logs/serializsers.py
@@ -13,5 +13,9 @@ def create(self, validated_data):
   return super().create(validated_data)
 
 def update(self, instance, validated_data):
+  # ! preventing editing after approval 
+  if instance.status == 'Aprroved':
+    raise serializers.validationError("Cannot edit approved log")
+  return super().update(instance, validated_data)
   
   
