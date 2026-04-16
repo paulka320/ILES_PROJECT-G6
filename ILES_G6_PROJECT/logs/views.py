641e37fa263f881d32739e3498b5a10e86a98e6d
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-04-16 11:03:09 +0300

    Add IsAuthenticated permission to views.py

--- ILES_G6_PROJECT/logs/views.py
+++ ILES_G6_PROJECT/logs/views.py
@@ -6,5 +6,6 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import weeklyLog
 from .serializers import WeeklyLogSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
