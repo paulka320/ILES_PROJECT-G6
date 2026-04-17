c70b3871029398837f566fc8f563d38c77d715e9
paulka320 <paulacou4@gmail.com>
2026-04-17 11:52:50 +0300

    included content on urls.py

--- ILES_G6_PROJECT/users/urls.py
+++ ILES_G6_PROJECT/users/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 from .views import RegisterView,UserListView,MyTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('register/',RegisterView.as_view()),
+    path('users/',UserListView.as_view()),
+    p
+]
