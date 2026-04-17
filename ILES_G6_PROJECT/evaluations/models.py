e0a2ba71f88b196c21cd8ae6e87392565808513e
nushifah <knushifah@gmail.com>
2026-04-17 20:54:43 +0300

    Add string representation for EvaluationCriteria

--- ILES_G6_PROJECT/evaluations/models.py
+++ ILES_G6_PROJECT/evaluations/models.py
@@ -6,3 +6,6 @@ User = settings.AUTH_USER_MODEL
 class EvaluationCriteria(models.model):
     name = models.CharField(max_length = 100)
     weight = models.FloatField()
+
+    def __str__(self):
+        return self.name
