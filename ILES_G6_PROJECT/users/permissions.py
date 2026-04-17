639fee1306395756b549f9f8c0c48784793aabc1
paulka320 <paulacou4@gmail.com>
2026-04-17 11:34:49 +0300

    included content in permissions.py

--- ILES_G6_PROJECT/users/permissions.py
+++ ILES_G6_PROJECT/users/permissions.py
@@ -1,1 +1,9 @@
 from rest_framework.permissions import BasePermission
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request,view):
+        return request.user.role=='admin'
+    
+class IsStudent(BasePermission):
+    def has_permission(self,request,view):
+        
