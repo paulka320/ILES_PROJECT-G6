a0a68dcc957c317c4036e51b4b5dc90b0395866e
joshkwizera11 <joshkwizera11>
2026-04-18 18:33:56 +0300

    Add urlpatterns definition in urls.py for internship routing

--- ILES_G6_PROJECT/internships/urls.py
+++ ILES_G6_PROJECT/internships/urls.py
@@ -2,3 +2,5 @@ from rest_framework.routers import DefaultRouter
 from .views import InternshipPlacementViewSet
 router = DefaultRouter()
 router.register(r'placements', InternshipPlacementViewSet)
+
+urlpatterns = router.urls
