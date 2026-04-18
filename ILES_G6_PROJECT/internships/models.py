c34842a045f101cfa32f345053032d5fc681caf6
joshkwizera11 <joshngoboka1@gmail.com>
2026-04-18 13:24:47 +0300

    Add InternshipPlacement model with student foreign key

--- ILES_G6_PROJECT/internships/models.py
+++ ILES_G6_PROJECT/internships/models.py
@@ -3,3 +3,5 @@ from django.conf import settings
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
+class InternshipPlacement(models.Model):
+    student = models.ForeignKey(User,on_delete=models.CASCADE,related_name='student_placements')
