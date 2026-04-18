7bc667ffda06e9fd3464c46eea1b66f08df18eb5
joshkwizera11 <joshkwizera11>
2026-04-18 13:28:23 +0300

    Add supervisor_name field to InternshipPlacement model

--- ILES_G6_PROJECT/internships/models.py
+++ ILES_G6_PROJECT/internships/models.py
@@ -6,3 +6,5 @@ User = settings.AUTH_USER_MODEL
 class InternshipPlacement(models.Model):
     student = models.ForeignKey(User,on_delete=models.CASCADE,related_name='student_placements')
     academic_supervisor = models.ForeignKey(User, on_delete = models.CASCADE, related_name= 'academic_students')
+    supervisor_name = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'supervisor_students')
+    start_date = models.DateField()
