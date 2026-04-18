490f297a5dce5c43e855f8eca04d9a351b99955a
joshkwizera11 <joshkwizera11>
2026-04-18 18:42:40 +0300

    Add Meta class definition to InternshipPlacementSerializer

--- ILES_G6_PROJECT/internships/serializers.py
+++ ILES_G6_PROJECT/internships/serializers.py
@@ -1,3 +1,5 @@
 from rest_framework import serializers
 from .models import InternshipPlacement
 class InternshipPlacementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InternshipPlacement
