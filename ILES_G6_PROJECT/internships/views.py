0f5012f54e306a222c5d2dc04206574b9222af74
joshkwizera11 <joshkwizera11>
2026-04-18 14:13:40 +0300

    Add permission_classes to SupervisorStudentsView for authentication

--- ILES_G6_PROJECT/internships/views.py
+++ ILES_G6_PROJECT/internships/views.py
@@ -14,3 +14,4 @@ class InternshipPlacementViewSet(viewsets.ModelViewSet):
 
 class SupervisorStudentsView(generics.ListAPIView):
     serializer_class = InternshipPlacementSerializer
+    permission_classes =[IsAuthenticated]
