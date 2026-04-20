42add0b4e77768bc18870ebfc9466aa3f432b12f
nushifah <knushifah@gmail.com>
2026-04-20 21:43:06 +0300

    Add IsAuthenticated permission for evaluation endpoints

--- ILES_G6_PROJECT/evaluations/views.py
+++ ILES_G6_PROJECT/evaluations/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Evaluation
 from .serializers import EvaluationSerializer
+from rest_framework.permissions import IsAuthenticated
