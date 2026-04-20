c087b325dc4ee969b7e39c83f48df853f6bc6f69
paulka320 <paulacou4@gmail.com>
2026-04-20 10:35:47 +0300

    tried using jsonResponse

--- ILES_G6_PROJECT/ILES_G6_PROJECT/urls.py
+++ ILES_G6_PROJECT/ILES_G6_PROJECT/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import JsonResponse
 
 def home (request):
     return JsonResponse({'message':'Welcome to the ILES API'})
