a60ee2f5fca7e33e4574f4efb6a0c6a6f1de4abd
paulka320 <paulacou4@gmail.com>
2026-04-20 10:33:18 +0300

    improved testing of the urls.py

--- ILES_G6_PROJECT/ILES_G6_PROJECT/urls.py
+++ ILES_G6_PROJECT/ILES_G6_PROJECT/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+def home (request):
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/',include('users.urls')),
