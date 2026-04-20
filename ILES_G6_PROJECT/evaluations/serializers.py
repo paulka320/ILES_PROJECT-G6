119af9ea241ed70401e0d1ac804b67d81451032d
nushifah <knushifah@gmail.com>
2026-04-20 12:35:29 +0300

    Prevent duplicate evaluations for the same student

--- ILES_G6_PROJECT/evaluations/serializers.py
+++ ILES_G6_PROJECT/evaluations/serializers.py
@@ -6,3 +6,13 @@ class EvaluationSerializer(serializers.ModelSerializer):
         model = Evaluation
         fields = '__all__'
         read_only_fields = ['total_score']
+
+    #prevent duplicate evaluation
+    def validate(self, data):
+        student = data['student']
+        evaluator = self.context['request'].user
+
+        if Evaluation.objects.filter(student=student, evaluator=evaluator).exists():
+            raise serializers.ValidationError("You have already evaluated this student.")
+        
+        return data
