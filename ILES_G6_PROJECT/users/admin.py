fcb8917e1651893bff5971e8e24d319d1e8ead51
paulka320 <paulacou4@gmail.com>
2026-04-20 10:10:15 +0300

    improved on admin.py

--- ILES_G6_PROJECT/users/admin.py
+++ ILES_G6_PROJECT/users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
 admin.site.register(CustomUser)
