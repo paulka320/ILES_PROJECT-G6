8b762cab6b404268731e16f73197b267d1ab86e5
paulka320 <paulacou4@gmail.com>
2026-04-20 10:34:36 +0300

    further testing

--- ILES_G6_PROJECT/ILES_G6_PROJECT/urls.py
+++ ILES_G6_PROJECT/ILES_G6_PROJECT/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 def home (request):
+    return JsonResponse({'message':'Welcome to the ILES API'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
