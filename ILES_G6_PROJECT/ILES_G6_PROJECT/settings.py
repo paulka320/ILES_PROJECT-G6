3793b45c8ac09c40d526f49b54b902745f29372f
paulka320 <paulacou4@gmail.com>
2026-04-20 10:24:22 +0300

    settings.py

--- ILES_G6_PROJECT/ILES_G6_PROJECT/settings.py
+++ ILES_G6_PROJECT/ILES_G6_PROJECT/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'users.CustomUser'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATED_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
