acf19fb6dd14a3122bf1242d4e8770098c92099b
paulka320 <paulacou4@gmail.com>
2026-04-20 11:01:09 +0300

    corrected the spelling of serializers

--- ILES_G6_PROJECT/logs/serializers.py
+++ ILES_G6_PROJECT/logs/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import WeeklyLog
 
-class WeeklyLogSerializser(serializers.ModelSerializer):
+class WeeklyLogSerializer(serializers.ModelSerializer):
   class meta:
     model = WeeklyLog
     fields ='__all__'
