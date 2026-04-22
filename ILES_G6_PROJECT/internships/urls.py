6c8bc33f739fb14d5255fba53fbcf36985b19415
paulka320 <paulacou4@gmail.com>
2026-04-23 00:22:44 +0300

    making corrections

--- ILES_G6_PROJECT/internships/urls.py
+++ ILES_G6_PROJECT/internships/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from .views import InternshipPlacementViewSet
 router = DefaultRouter()
-router.register(r'placements', InternshipPlacementViewSet)
+router.register(r'placements', InternshipPlacementViewSet,basename='placements')
 
 urlpatterns = router.urls
