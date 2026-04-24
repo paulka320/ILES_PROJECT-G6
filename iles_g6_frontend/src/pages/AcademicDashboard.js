8bc623d8d00b502a6fd4dd02e984585f9e3b1c8d
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-04-24 16:09:04 +0300

    Update AcademicDashboard.js

--- iles_g6_frontend/src/pages/AcademicDashboard.js
+++ iles_g6_frontend/src/pages/AcademicDashboard.js
@@ -69,6 +69,17 @@ const AcademicDashboard = () =>{
   <ResponseiveContainer width="100%" height={300}>
       <LineChart data={chartData}>
          <CartesianGrid strokeDasharray="33"/>
+          <XAxis dataKey="student"/>
+         <YAxis/>
+        <Tooltip/>
+      <Line type="monotone" dataKey="score" stroke="#8884d8" strokeWidth={3}/>
+   </LineChart>
+  </ResponsiveContainer>
+ ):(
+   <p>No evalutions data yet</p>
+   )}
+
+ 
   
                      
                      
