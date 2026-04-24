a12f38842367077efd7452b8790161b83a0d3313
nushifah <knushifah@gmail.com>
2026-04-24 19:22:44 +0300

    Import chart components for evaluation scores

--- iles_g6_frontend/src/pages/StudentDashboard.js
+++ iles_g6_frontend/src/pages/StudentDashboard.js
@@ -1,3 +1,4 @@
 import { useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { getStudentLogs, getStudentPlacement, getStudentEvaluations } from "../api/student";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
