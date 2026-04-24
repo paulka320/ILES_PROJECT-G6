c94195fcf1a992114bc3fc66ac0efeb05d2286d2
joshkwizera11 <joshkwizera11>
2026-04-24 23:45:43 +0300

    Import Bootstrap components in SupervisorDashboard

--- iles_g6_frontend/src/pages/SupervisorDashboard.js
+++ iles_g6_frontend/src/pages/SupervisorDashboard.js
@@ -2,3 +2,4 @@
 import { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../auth/AuthContext";
 import API from "../api/axios";
+import { Container, Row, Col, Card, Table, Badge } from "react-bootstrap";
