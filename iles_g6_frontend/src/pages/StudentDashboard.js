60487c81c405ea7357ef71818424951d12056c27
nushifah <knushifah@gmail.com>
2026-04-24 19:24:57 +0300

    Import bootstrap components for dashboard layout

--- iles_g6_frontend/src/pages/StudentDashboard.js
+++ iles_g6_frontend/src/pages/StudentDashboard.js
@@ -2,3 +2,4 @@ import { useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { getStudentLogs, getStudentPlacement, getStudentEvaluations } from "../api/student";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import { Card, Container, Row, Col, Table, Badge } from "react-bootstrap";
