b0efaa8694fed777b45147264c2af1f150f13bac
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-04-24 14:33:28 +0300

    Update AcademicDashboard.js

--- iles_g6_frontend/src/pages/AcademicDashboard.js
+++ iles_g6_frontend/src/pages/AcademicDashboard.js
@@ -2,6 +2,7 @@
 import {userEffect, useState, useContext} from "react";
 import {AuthContext} from "../auth/AuthContext";
 import API from "../api/axios";
+import {container, Row, Col, card,Table,Badge} from "react-bootstrap";
 
 
 
