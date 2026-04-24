e872468fd98c87536f838bacefd88cee74f9b28f
nushifah <knushifah@gmail.com>
2026-04-24 19:32:24 +0300

    Create StudentDashboard component

--- iles_g6_frontend/src/pages/StudentDashboard.js
+++ iles_g6_frontend/src/pages/StudentDashboard.js
@@ -3,3 +3,5 @@ import { AuthContext } from "../context/AuthContext";
 import { getStudentLogs, getStudentPlacement, getStudentEvaluations } from "../api/student";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 import { Card, Container, Row, Col, Table, Badge } from "react-bootstrap";
+
+const StudentDashboard = () => {
