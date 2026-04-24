6943fb18e7708d461fb61110e2a41ea6fb3b5510
joshkwizera11 <joshkwizera11>
2026-04-24 23:46:54 +0300

    Import recharts components in SupervisorDashboard

--- iles_g6_frontend/src/pages/SupervisorDashboard.js
+++ iles_g6_frontend/src/pages/SupervisorDashboard.js
@@ -3,3 +3,4 @@ import { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../auth/AuthContext";
 import API from "../api/axios";
 import { Container, Row, Col, Card, Table, Badge } from "react-bootstrap";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
