d00f4e5f946c1c82742e04d99933c33bddbf57fc
nushifah <knushifah@gmail.com>
2026-04-30 13:17:19 +0300

    Display submitted logs statistic

--- iles_g6_frontend/src/pages/StudentDashboard.js
+++ iles_g6_frontend/src/pages/StudentDashboard.js
@@ -72,6 +72,15 @@ const StudentDashboard = () => {
                     <Card className="text-center p-3">
                         <h5>Total Logs</h5>
                         <h2>{totalLogs}</h2>
+                    </Card>
+                </Col>
+                <Col md={4}>
+                    <Card className="text-center p-3">
+                        <h5>Submitted Logs</h5>
+                        <h2>{submittedLogs}</h2>
+
+
+
 
 
         
