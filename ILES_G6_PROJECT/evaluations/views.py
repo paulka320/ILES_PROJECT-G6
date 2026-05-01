1b11fc64aad520c7c24e978d790a5707f6012d0f
paulka320 <paulacou4@gmail.com>
2026-05-01 13:28:56 +0300

    imported Avg from django.db.models

--- ILES_G6_PROJECT/evaluations/views.py
+++ ILES_G6_PROJECT/evaluations/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.db.models import Avg
 from rest_framework import viewsets
 from .models import Evaluation
 from .serializers import EvaluationSerializer
