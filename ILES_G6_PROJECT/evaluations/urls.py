c9756e9c0e63c45cd3616634227dc27f9e227aa5
paulka320 <paulacou4@gmail.com>
2026-05-01 13:41:38 +0300

    added AcademicEvaluationsView

--- ILES_G6_PROJECT/evaluations/urls.py
+++ ILES_G6_PROJECT/evaluations/urls.py
@@ -5,7 +5,7 @@ router = DefaultRouter()
 router.register(r'evaluations', EvaluationViewSet,basename='evaluations')
 
 urlpatterns = router.urls + [
-    path('academic/<int:id>/evaluations/',AcademicEvaluaionsView.as_view(),name="academic-evaluations"),
+    path('academic/<int:id>/evaluations/',AcademicEvaluationsView.as_view(),name="academic-evaluations"),
     path('academic/<int:id>/stats/',academic_stats,name="academic-stats"),
     path('admin/evaluations/',AdminEvaluationsView.as_view(),name="admin-evaluations"),
 ]
