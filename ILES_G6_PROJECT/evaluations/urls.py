5a1587ef6988323bbae1ba1bfe84dc4b20cfe911
paulka320 <paulacou4@gmail.com>
2026-05-01 12:00:34 +0300

    improving evaluations/urls.py

--- ILES_G6_PROJECT/evaluations/urls.py
+++ ILES_G6_PROJECT/evaluations/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import EvaluationViewSet
+from .views import EvaluationViewSet,AcademicEvaluationsView,academic_stats,AdminEvaluationsView
 
 router = DefaultRouter()
 router.register(r'evaluations', EvaluationViewSet,basename='evaluations')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    
+]
