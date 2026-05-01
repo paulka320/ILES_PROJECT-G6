eaf57ee28e7bc9bef3b40901d5b7827af75ba86d
paulka320 <paulacou4@gmail.com>
2026-05-01 15:32:57 +0300

    improving on the urls.py

--- ILES_G6_PROJECT/users/urls.py
+++ ILES_G6_PROJECT/users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import RegisterView,UserListView,MyTokenObtainPairView
+from .views import RegisterView,MyTokenObtainPairView,AdminStatsView,AdminUserViewSet
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
+
+router = DefaultRouter()
+router.register(r"admin/users", AdminUserViewSet, basename='admin-users')
+urlpatterns =router.urls + [
     path('register/',RegisterView.as_view()),
-    path('users/',UserListView.as_view()),
     path('login/',MyTokenObtainPairView.as_view()),
     path('refresh/',TokenRefreshView.as_view()),
 ]
