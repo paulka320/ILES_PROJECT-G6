2bd7f60da72545845176b401bcc58af6ef46a9e7
paulka320 <paulacou4@gmail.com>
2026-05-01 14:26:09 +0300

    added views to urls.py

--- ILES_G6_PROJECT/logs/urls.py
+++ ILES_G6_PROJECT/logs/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import WeeklyLogViewSet
+from .views import WeeklyLogViewSet,SupervisorPendingLogsView,AcademicStudentLogsView,AdminLogsView
 
 router =DefaultRouter()
 router.register(r'logs', WeeklyLogViewSet)
