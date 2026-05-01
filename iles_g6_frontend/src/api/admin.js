6bf9f33c12f152e7156555e2f4d18436bd41ffaf
paulka320 <paulacou4@gmail.com>
2026-05-01 11:24:46 +0300

    added placements.js

--- iles_g6_frontend/src/api/admin.js
+++ iles_g6_frontend/src/api/admin.js
@@ -5,3 +5,6 @@ export const getAllUsers = () =>
 
 export const deleteUser = (id) =>
     API.delete(`/users/admin/users/${id}/`);
+
+export const getAllPlacements = () =>
+    API.get("/internships/internshipplacement");
