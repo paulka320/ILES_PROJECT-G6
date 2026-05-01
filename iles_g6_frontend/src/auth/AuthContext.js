3e540ac6f1fea8ce5504241d058d2dd3e6109003
paulka320 <paulacou4@gmail.com>
2026-05-01 10:57:05 +0300

    Content addition

--- iles_g6_frontend/src/auth/AuthContext.js
+++ iles_g6_frontend/src/auth/AuthContext.js
@@ -1,3 +1,21 @@
 import { createContext, useState } from "react";
 import API from "../api/axios";
 import { jwtDecode } from "jwt-decode";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    const login = async (username, password) => {
+        const res = await API. post("users/login/", { username, password });
+
+        localStorage.setItem("token", res.data.access);
+
+        setUser({
+            id:res.data.id,
+            username:res.data.username,
+            role:res.data.role
+        });
+    }
+}
