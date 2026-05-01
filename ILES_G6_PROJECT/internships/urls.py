4acc1f26a2649804dca7d8c7878dc775611176cd
paulka320 <paulacou4@gmail.com>
2026-05-01 13:52:49 +0300

    added admin-placements

--- ILES_G6_PROJECT/internships/urls.py
+++ ILES_G6_PROJECT/internships/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
-from .views import InternshipPlacementViewSet
+from .views import InternshipPlacementViewSet,SupervisorStudentsView,AcademicStudentsView,AdminPlacementViewSet
 router = DefaultRouter()
-router.register(r'placements', InternshipPlacementViewSet,basename='placements')
+router.register(r'admin/placements', AdminPlacementViewSet,basename='admin-placements')
 
 urlpatterns = router.urls
