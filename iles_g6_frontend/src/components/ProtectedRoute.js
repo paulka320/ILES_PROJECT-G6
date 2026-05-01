3b90be6b0ad24b01333bdff538c99f58bb18311f
paulka320 <paulacou4@gmail.com>
2026-05-01 11:07:07 +0300

    finished setting ProtectedRoutes.js

--- iles_g6_frontend/src/components/ProtectedRoute.js
+++ iles_g6_frontend/src/components/ProtectedRoute.js
@@ -1,3 +1,12 @@
 import { useContext } from "react";
 import { AuthContext } from "../auth/AuthContext";
 import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+    const { user } = useContext(AuthContext);
+
+    return user ? children : <Navigate to = "/" />;
+
+};
+
+export default ProtectedRoute;
