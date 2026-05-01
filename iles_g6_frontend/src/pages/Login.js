a24047f7e99f2db70c269ec3bded13dcf777e66a
paulka320 <paulacou4@gmail.com>
2026-05-01 10:33:31 +0300

    added login variable

--- iles_g6_frontend/src/pages/Login.js
+++ iles_g6_frontend/src/pages/Login.js
@@ -1,3 +1,13 @@
 import { useState, useEffect } from "react";
 import { AuthContext } from  "../auth/AuthContext";
 import {useNavigate} from "react-router-dom";
+
+const Login = () => {
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const [form, setForm] = useState ({
+        username: "",
+        password: "",
+    });
+}
