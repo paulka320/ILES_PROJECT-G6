f597016eb73f8b0b2e38c9077cad736f017df026
paulka320 <paulacou4@gmail.com>
2026-05-01 14:29:51 +0300

    added views into urls.py

--- ILES_G6_PROJECT/logs/urls.py
+++ ILES_G6_PROJECT/logs/urls.py
@@ -2,6 +2,10 @@ from rest_framework.routers import DefaultRouter
 from .views import WeeklyLogViewSet,SupervisorPendingLogsView,AcademicStudentLogsView,AdminLogsView
 
 router =DefaultRouter()
-router.register(r'logs', WeeklyLogViewSet)
+router.register(r'logs', WeeklyLogViewSet,basename='weeklylogs')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('supervisor/pending/',SuperVisorPendingLogsView.as_view(),name="supervisor-pending-logs"),
+    path('academic/<int:student_id>/logs/',AcademicStudentLogsView.as_view(),name = "academic-student-logs"),
+    
+]
