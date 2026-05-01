7a406416089292b415f4b1722c3df8a95e67275b
paulka320 <paulacou4@gmail.com>
2026-05-01 14:24:22 +0300

    imrpved serializers.py

--- ILES_G6_PROJECT/logs/serializers.py
+++ ILES_G6_PROJECT/logs/serializers.py
@@ -9,16 +9,17 @@ class UserMiniSerializer(serializers.ModelSerializer):
     fields = ['id','username','role']
 
 class WeeklyLogSerializer(serializers.ModelSerializer):
-  
+  student = UserMiniSerializer(read_only=True)
+
   class meta:
     model = WeeklyLog
     fields ='__all__'
-    read_only_fields = ['status', 'student']
+    read_only_fields = ['status', 'student','supervisor_comment']
 
 def create(self, validated_data):
-  user = self.context['request'].user
-  validated_data['student'] = user
-  return super().create(validated_data)
+  request = self.context['request']
+  validated_data['student'] = request.user
+  return WeeklyLog.objects.create(**validated_data)
 
 def update(self, instance, validated_data):
   # ! preventing editing after approval 
