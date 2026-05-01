a0dd9d881a9e64cb2477b16e9af1cea1daae688a
paulka320 <paulacou4@gmail.com>
2026-05-01 16:03:16 +0300

    added settings.py

--- ILES_G6_PROJECT/ILES_G6_PROJECT/settings.py
+++ ILES_G6_PROJECT/ILES_G6_PROJECT/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +130,4 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
