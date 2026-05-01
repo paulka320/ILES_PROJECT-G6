ca79d3c702a9ded3e9cd44b61427c6ea733acdfc
paulka320 <paulacou4@gmail.com>
2026-05-01 10:15:16 +0300

    added protected routes

--- iles_g6_frontend/src/App.js
+++ iles_g6_frontend/src/App.js
@@ -38,6 +38,14 @@ function App() {
               </ProtectedRoute>
             }
           />
+          <Route
+            path="/admin"
+            element = {
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
 
         </Routes>
 
