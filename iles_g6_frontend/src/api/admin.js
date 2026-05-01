84a35262a6143684d272ed57d2f6c3aae7d99b24
paulka320 <paulacou4@gmail.com>
2026-05-01 11:22:47 +0300

    included delete in admin.js

--- iles_g6_frontend/src/api/admin.js
+++ iles_g6_frontend/src/api/admin.js
@@ -2,3 +2,6 @@ import API from "./axios";
 
 export const getAllUsers = () => 
     API.get("/users/admin/users/");
+
+export const deleteUser = (id) =>
+    API.delete(`/users/admin/users/${id}/`);
