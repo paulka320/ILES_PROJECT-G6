a6cbe843f97380718fdf9d74ce4b47d590444bcb
nushifah <knushifah@gmail.com>
2026-05-02 21:55:33 +0300

    Add Recharts components for data visualization

--- iles_g6_frontend/src/pages/AcademicDashboard.js
+++ iles_g6_frontend/src/pages/AcademicDashboard.js
@@ -3,4 +3,5 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../auth/AuthContext';
 import API from '../api/axios';
 import { Container, Row, Col, Card, Table, Button, Form, Badge, Alert, Modal, Tab, Nav } from 'react-bootstrap';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, BarChart, Bar } from 'recharts';
 
