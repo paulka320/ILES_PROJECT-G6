fbbf616b37058da750d850be022390375dd5a178
nushifah <knushifah@gmail.com>
2026-05-02 21:54:10 +0300

    Import React Bootstrap UI components

--- iles_g6_frontend/src/pages/AcademicDashboard.js
+++ iles_g6_frontend/src/pages/AcademicDashboard.js
@@ -2,4 +2,5 @@ import { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../auth/AuthContext';
 import API from '../api/axios';
+import { Container, Row, Col, Card, Table, Button, Form, Badge, Alert, Modal, Tab, Nav } from 'react-bootstrap';
 
