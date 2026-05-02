3fb6719c7b9ef06316bb35f1bef6c660f3cbb7bc
joshkwizera11 <joshngoboka1@gmail.com>
2026-05-02 21:43:56 +0300

    Add import for AcademicDashboard in Dashboard.js

--- iles_g6_frontend/src/pages/Dashboard.js
+++ iles_g6_frontend/src/pages/Dashboard.js
@@ -3,3 +3,4 @@ import { AuthContext } from "../auth/AuthContext";
 
 import StudentDashboard from "./StudentDashboard";
 import SupervisorDashboard from "./SupervisorDashboard";
+import AcademicDashboard from "./AcademicDashboard";
