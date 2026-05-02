1c8af61d632966ef48c326756d58d2c7fadf1f68
nushifah <knushifah@gmail.com>
2026-05-02 21:50:47 +0300

    Import API instance for backend communication

--- iles_g6_frontend/src/pages/AcademicDashboard.js
+++ iles_g6_frontend/src/pages/AcademicDashboard.js
@@ -1,3 +1,5 @@
 import { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../auth/AuthContext';
+import API from '../api/axios';
+
