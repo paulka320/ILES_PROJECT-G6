1b43f6a0189b1502924a175224871acd69c97b6a
nushifah <knushifah@gmail.com>
2026-05-02 21:59:04 +0300

    Initialize evaluations state

--- iles_g6_frontend/src/pages/AcademicDashboard.js
+++ iles_g6_frontend/src/pages/AcademicDashboard.js
@@ -8,4 +8,5 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContai
 const AcademicDashboard = () => {
     const { user } = useContext(AuthContext);
     const [students, setStudents] = useState([]);
+    const [placements, setPlacements] = useState([]);
 
