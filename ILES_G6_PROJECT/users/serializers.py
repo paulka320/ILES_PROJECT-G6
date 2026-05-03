678757c425348293477da52ce0cea4be4cb66195
paulka320 <paulacou4@gmail.com>
2026-05-03 17:38:50 +0300

    upgraded serializers.py

--- ILES_G6_PROJECT/users/serializers.py
+++ ILES_G6_PROJECT/users/serializers.py
@@ -9,10 +9,16 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True,required=True)
+    role= serializers.ChoiceField(choices=CustomUser.ROLE_CHOICES, default='student')
 
     class Meta:
         model = CustomUser
         fields = ['username','email','password','role']
+
+        def validate_role(self, value):
+            if value not in ['student','supervisor','academic']:
+                raise serializers.ValidationError("Invalid role for registration.")
+            return value
 
         def create(self,validated_data):
             role = validated_data.pop('role','student')
