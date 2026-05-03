758a61d1a0196c9b82a355e80c6196d832a08fb8
paulka320 <paulacou4@gmail.com>
2026-05-03 17:18:18 +0300

    edited views.py

--- ILES_G6_PROJECT/notifications/views.py
+++ ILES_G6_PROJECT/notifications/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Notification
 # Create your views here.
+from .serializers import NotificationSerializer
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Notification.objects.all()
+
+    def get_queryset(self):
+        return Notification.objects.filter(recipient=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(recipient=self.request.user)
