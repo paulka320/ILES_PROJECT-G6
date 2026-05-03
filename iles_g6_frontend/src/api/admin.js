553063cf006f2854b1f0f95f1ff6abadc6e89c2e
paulka320 <paulacou4@gmail.com>
2026-05-03 12:04:55 +0300

    debugging admin.js

--- iles_g6_frontend/src/api/admin.js
+++ iles_g6_frontend/src/api/admin.js
@@ -4,7 +4,7 @@ export const getAllUsers = () =>
     API.get("users/admin/users/");
 
 export const deleteUser = (id) =>
-    API.delete(`/users/admin/users/${id}/`);
+    API.delete(`users/admin/users/${id}/`);
 
 export const getAllPlacements = () =>
     API.get("/internships/internshipplacement");
