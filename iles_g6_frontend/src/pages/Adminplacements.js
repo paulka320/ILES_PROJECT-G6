bdc91e6195cd6906f7bd1b7778f6a2213e89c45d
paulka320 <paulacou4@gmail.com>
2026-05-03 12:58:26 +0300

    including Adminplacements

---
+++ iles_g6_frontend/src/pages/Adminplacements.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import API from "../api/axios";
+
+import { Container, Table, Card, Row, Col, } from "react-bootstrap";
+
+
+const  AdminPlacements = () => {
+    
+    const [placements, setPlacements] = 
+        useState([]);
+    
+        const fetchPlacements = async () => {
+
+            try {
+                const res = 
+                    await API.get(
+                        "internships/admin/placements/"
+                    );
+                setPlacements(res.data);
+            } catch (err) {}
+        }
+}
