cf86f31c4f59e4c5504e94e41c3a55516804e929
paulka320 <paulacou4@gmail.com>
2026-05-03 16:28:11 +0300

    added models.py

--- ILES_G6_PROJECT/notifications/models.py
+++ ILES_G6_PROJECT/notifications/models.py
@@ -16,4 +16,17 @@ class Notification(models.Model):
     notification_type = models.CharField(max_length=20,choices=NOTIFICATION_TYPES)
     title = models.CharField(max_length=255)
     message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    related_log = models.ForeignKey('logs.WeeklyLog',on_delete=models.CASCADE, null=True, blank=True)
+    related_evaluation = models.ForeignKey('evaluations.Evaluation',on_delete=models.CASCADE, null=True,blank=True)
+    related_placement = models.ForeignKey('internships.InternshipPlacement', on_delete=models.CASCADE, null=True,blank=True)
+
+    def __str__(self):
+        return f"{self.recipient.username}-{self.title}"
+    
+
+class Meta:
+    ordering = ['-created_at']
     
