a2dfa2895ba3538d2166e31451f70f5f27f9bf91
paulka320 <paulacou4@gmail.com>
2026-05-03 15:59:39 +0300

    improved the views.py

--- ILES_G6_PROJECT/logs/views.py
+++ ILES_G6_PROJECT/logs/views.py
@@ -89,11 +89,11 @@ def admin_approve(self,request,pk=None):
 
 @action (detail=True, methods = ["post"], url_path="admin_reject")
 def admin_reject(self,request,pk = None):
-  log = self.get_object()
+  
 
   if request.user.role != "admin":
     return Response({"error":"Unauthorized"},status=403)
-  
+  log = self.get_object()
   log.status = "rejected"
   log.save()
 
