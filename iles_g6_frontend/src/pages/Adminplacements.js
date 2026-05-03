ad679a08c7657f9b2a309def4d2b4dea4ce5a566
paulka320 <paulacou4@gmail.com>
2026-05-03 13:08:04 +0300

    added placements

--- iles_g6_frontend/src/pages/Adminplacements.js
+++ iles_g6_frontend/src/pages/Adminplacements.js
@@ -29,4 +29,32 @@ const  AdminPlacements = () => {
         fetchPlacements();
     }, []);
 
+    return (
+<Container fluid className="p-4">
+    <Row>
+        <Col>
+        <Card className="p-3">
+            <h4>All Placements</h4>
+            <Table striped bordered>
+                <thead>
+                    <tr>
+                        <th>Student</th>
+                        <th>Company</th>
+                        <th>Supervisor</th>
+                        <th>Academic</th>
+                    </tr>
+                </thead>
+
+                {placements.map((p) =>(
+                    <tr key ={p.id}>
+                        <td></td>
+                    </tr>
+                ))}
+            </Table>
+        </Card>
+        </Col>
+    </Row>
+</Container>
+    )
+
 }
