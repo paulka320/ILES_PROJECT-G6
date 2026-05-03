543dd7a6414c15f2a495999c9a64d36357b3ca3e
paulka320 <paulacou4@gmail.com>
2026-05-03 12:15:11 +0300

    added assignsupervisor

--- iles_g6_frontend/src/api/admin.js
+++ iles_g6_frontend/src/api/admin.js
@@ -13,6 +13,11 @@ export const getAllPlacements = () => {
     console.log("API CALL:GET PLACEMENTS");
     API.get("internships/admin/placements/");
 };
+
+export const assignSupervisor = (placementId, supervisorId) =>
+    API.post(`internships/admin/placements/${placementId}/assign_academic_supervisor/`, {
+        supervisor_id:supervisorId,
+    });
 export const getAllLogs = () =>
     API.get("/logs/weeklylogs/");
 
