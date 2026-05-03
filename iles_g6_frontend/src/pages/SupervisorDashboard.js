00dca09e7415b4e03e46b364ae37c70d1a3b3929
paulka320 <paulacou4@gmail.com>
2026-05-03 13:30:01 +0300

    added import { Link } from react-router-dom to supervisor dashboard

--- iles_g6_frontend/src/pages/SupervisorDashboard.js
+++ iles_g6_frontend/src/pages/SupervisorDashboard.js
@@ -2,8 +2,11 @@
 import { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../auth/AuthContext";
 import API from "../api/axios";
-import { Container, Row, Col, Card, Table, Badge } from "react-bootstrap";
+import { Container, Row, Col, Card, Table, Badge, Button, Form, Alert } from "react-bootstrap";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+import { Link } from "react-router-dom";
+
+
 
 const SupervisorDashboard = () => {
   const { user } = useContext(AuthContext);
