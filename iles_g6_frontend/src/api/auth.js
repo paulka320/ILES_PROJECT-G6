54771da7f10250d7690ebbfea971c513e80d75ff
paulka320 <paulacou4@gmail.com>
2026-05-03 12:00:39 +0300

    added values on auth.js

---
+++ iles_g6_frontend/src/api/auth.js
@@ -0,0 +1,9 @@
+import API from "./axios";
+
+export const login = async (formData) => {
+    const res = await API.post("/users/login/",formData);
+
+    localStorage.setItem("token",res.data.access);
+
+    return 
+}
