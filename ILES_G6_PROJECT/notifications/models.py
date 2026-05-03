245460ed1ab77e2eea7c325e9945f8f642409c4c
paulka320 <paulacou4@gmail.com>
2026-05-03 16:20:34 +0300

    includes models.py

--- ILES_G6_PROJECT/notifications/models.py
+++ ILES_G6_PROJECT/notifications/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Notification(models.Model):
+    NOTIFICATION_TYPES = (
+        ('log_submitted','Log Submitted'),
+        ('log_approved','Log Approved'),
+        ('log_rejected','Log Rejected'),
+        ('evaluation_created','Evaluation Created'),
+        ('placement_assigned','Placement Assigned'),
+    )
+
+    recipient = models.ForeignKey(User,on_delete=models.CASCADE,related_name='notifications')
+    notification_type = models.CharField(max_length=20,choices=NOTIFICATION_TYPES)
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+    
