135d32c5f399569f548e4509e588c8224f5eedc0
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-05-04 09:29:15 +0300

    Update Notifications.js

--- iles_g6_frontend/src/pages/Notifications.js
+++ iles_g6_frontend/src/pages/Notifications.js
@@ -2,3 +2,7 @@ import { useEffect, useState, useContext } from 'react';
 import { AuthContext } from '../context/AuthContext'; 
 import API from '../api/axios'; 
 import { Container, Row, Col, Card, Button, Table, Badge, Alert, Pagination } from 'react-bootstrap';
+
+const Notifications = () => {
+  const { user} = useContext(AuthContext);
+  
