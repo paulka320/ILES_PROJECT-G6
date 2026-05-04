3b3169da95d34c2b924209f62a516cd657e7f3a1
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-05-04 11:27:06 +0300

    Import student-related API functions in StudentDashboard

--- iles_g6_frontend/src/pages/StudentDashboard.js
+++ iles_g6_frontend/src/pages/StudentDashboard.js
@@ -2,6 +2,11 @@ import { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../auth/AuthContext";
 import API from "../api/axios";
+import {
+  getStudentLogs,
+  getStudentPlacement,
+  getStudentEvaluations,
+} from "../api/student";
 
 
 const StudentDashboard = () => {
