a2bb3d45d2e8a725002dffddf2ab941f41cdf211
joshkwizera11 <joshngoboka1@gmail.com>
2026-05-04 15:41:59 +0300

    Fix: Add conditional rendering for loading message in Login form

--- iles_g6_frontend/src/pages/Login.js
+++ iles_g6_frontend/src/pages/Login.js
@@ -162,3 +162,4 @@ const Login = () => {
                   <Spinner animation="border" size="sm" className="me-2" />
                   {isRegister ? "Registering..." : "Signing in..."}
                 </>
+              ) : (
