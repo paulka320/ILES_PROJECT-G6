d9325aa62b292c9d4438ee2f5b454c6bb627e11b
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-05-04 11:28:34 +0300

    Update StudentDashboard.js

--- iles_g6_frontend/src/pages/StudentDashboard.js
+++ iles_g6_frontend/src/pages/StudentDashboard.js
@@ -2,11 +2,22 @@ import { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../auth/AuthContext";
 import API from "../api/axios";
+
 import {
   getStudentLogs,
   getStudentPlacement,
   getStudentEvaluations,
 } from "../api/student";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 
 const StudentDashboard = () => {
