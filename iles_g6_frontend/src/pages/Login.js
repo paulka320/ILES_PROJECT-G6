98f20699e5581194e52eab795f391f55c688475c
joshkwizera11 <joshngoboka1@gmail.com>
2026-05-04 11:59:46 +0300

    Add missing import for AuthContext in Login component

--- iles_g6_frontend/src/pages/Login.js
+++ iles_g6_frontend/src/pages/Login.js
@@ -1,3 +1,4 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Card, Form, Button, Container, Alert, Spinner } from "react-bootstrap";
+import { AuthContext } from "../auth/AuthContext";
