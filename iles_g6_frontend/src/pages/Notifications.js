2f02e69a54fdb276e24db836528523318f87df1f
LOMER Francis Peter <106156684+lomerfp@users.noreply.github.com>
2026-05-04 10:25:03 +0300

    Update Notifications.js

--- iles_g6_frontend/src/pages/Notifications.js
+++ iles_g6_frontend/src/pages/Notifications.js
@@ -150,7 +150,7 @@ const getNotificationBadgeColor = (type) => {
                         </div>
                     </Card>
                 </Col>
-            </Row
+            </Row>
                                 
 
 
