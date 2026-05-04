9147a475ceeb365ed8af5d271d43ae5b5a0b69b6
joshkwizera11 <joshngoboka1@gmail.com>
2026-05-04 15:25:36 +0300

    Fix: Add new Form.Group for password input in Login.js

--- iles_g6_frontend/src/pages/Login.js
+++ iles_g6_frontend/src/pages/Login.js
@@ -125,3 +125,5 @@ const Login = () => {
                 <Form.Text className="text-muted">We&apos;ll never share your email.</Form.Text>
               </Form.Group>
             )}
+
+            <Form.Group className="mb-4" controlId="loginPassword">
