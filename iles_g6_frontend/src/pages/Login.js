fbb2d26224ff35df7adc8a109fe8aa626c69b0cd
joshkwizera11 <joshngoboka1@gmail.com>
2026-05-04 14:59:42 +0300

    modified Login.js

--- iles_g6_frontend/src/pages/Login.js
+++ iles_g6_frontend/src/pages/Login.js
@@ -77,4 +77,5 @@ const Login = () => {
         <Card.Body className="p-4">
           <div className="text-center mb-4">
             <div className="mb-2">
+              <span className="badge bg-primary rounded-pill px-3 py-2">ILES</span>
   
