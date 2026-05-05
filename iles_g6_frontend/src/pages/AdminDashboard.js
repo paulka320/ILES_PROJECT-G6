9d16d6cfc2a3152ecf67c65a8fe9cbba2c5f8fde
nushifah <knushifah@gmail.com>
2026-05-05 12:25:06 +0300

    Import admin API utilities

--- iles_g6_frontend/src/pages/AdminDashboard.js
+++ iles_g6_frontend/src/pages/AdminDashboard.js
@@ -1,2 +1,17 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { 
+  getAdminStats,
+  getAllUsers,
+  getAllPlacements,
+  getAllLogs,
+  getAllEvaluations,
+  deleteUser,
+  deleteLog,
+  approveLog,
+  rejectLog,
+  assignSupervisor,
+  assignAcademicSupervisor,
+  createPlacement,
+  updateUserRole,
+} from "../api/admin";
