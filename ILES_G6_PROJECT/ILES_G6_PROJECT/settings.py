d24b6a3d3bbd5769d57aa38f1b257226b96364d5
joshkwizera11 <joshngoboka1@gmail.com>
2026-06-10 15:45:55 +0300

    Update compiled Python files and database
    - Updated various .pyc files in the __pycache__ directories for ILES_G6_PROJECT, evaluations, internships, logs, notifications, and users.
    - Modified db.sqlite3 to reflect recent changes in the database schema.
    - Ensured all migrations are up to date with the latest changes in models and serializers.

--- ILES_G6_PROJECT/ILES_G6_PROJECT/settings.py
+++ ILES_G6_PROJECT/ILES_G6_PROJECT/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'ILES_G6_PROJECT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ILES_G6_db',
+        'USER':'postgres',
+        'PASSWORD':'joshorcus247',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
